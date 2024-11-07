2bdc52375bb27493058e0b3fe642111d4d22a3ea
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-11-06 21:43:38 -0800

    second - done with pseudocode

--- main.cpp
+++ main.cpp
@@ -1,1 +1,51 @@
 //PSEUDOCODE FOR PROJECT -- lab 29
+
+//include iostream and other libraries
+//using namespace std
+
+//function which will read data from file
+    //open file for reading
+    //if file opens then for each line
+        //read in patient name, age, condition and department they need to go to
+        //if the patient to that department
+    //close file
+    //else
+        //error opening file
+
+//function which will add patient to dept
+    //cout ask patient name
+    //locate dept and find patient in list
+    //possibly change of condition
+    //add cout line which tells who got added to where
+
+//function to transfer patient to different dept
+    //cout ask patient name
+    //cout ask dept
+    // find patient in list and transfer them
+    //add cout line which tells console who got moved to where
+
+//function to discharge
+    //ask name
+    //ask dept
+    //if patient is in the discharge list then move them out
+    //cout who got removed
+
+//display function
+    //for loop for each department
+    //output number of patients with their conditions?
+    //output patient names in each condition list
+
+
+//main function?
+    //display initial hospital dept data
+
+    //forloop for each day, 30 days total
+        //print cout message of which day we are in
+        //for each of the depts
+            //add new pt, update condition, transfer, or discharge
+            //^ still deciding if this should be based on random probability
+
+        //display updated dept data
+        //maybe print summary
+        
+    //return 0 :)
